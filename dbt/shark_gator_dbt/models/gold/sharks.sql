33958a44ae33f9b76ef07cb2df150706682a8f9c
Helen Esterman <helenesterman99@gmail.com>
2025-09-10 15:26:01 -0400

    gold layer completed

---
+++ dbt/shark_gator_dbt/models/gold/sharks.sql
@@ -0,0 +1,40 @@
+{{config(
+    materialized='table',
+    tags= ['models', 'gold', 'sharks', 'monthly'],
+    schema='GOLD'
+)}}
+
+SELECT DISTINCT
+    SA.ATTACK_ID,
+    SA.DATE_CLEAN AS ATTACK_DATE,
+    SA.YEAR_CLEAN AS ATTACK_YEAR,
+    SA.ATTACK_TYPE,
+    SA.COUNTRY_CLEAN AS COUNTRY,
+    SA.FATAL_OUTCOME,
+    SL.AREA,
+    SL.LOCATION,
+    SL.SPECIES,
+    RS.ASSESSMENT_ID,
+    RS.TAXON_ID,
+    RS.SCIENTIFIC_NAME,
+    RS.ORDER_NAME,
+    RS.GENUS_NAME,
+    RS.SPECIES_NAME,
+    RS.COMMON_NAME,
+    RS.RED_LIST_CATEGORY,
+    RS.YEAR_PUBLISHED AS RED_LIST_YEAR,
+    RS.ASSESSMENT_DATE,
+    RS.HABITAT,
+    RS.THREATS,
+    RS.POPULATION,
+    RS.POPULATION_TREND,
+    RS.GLOBAL_LOCATION_RANGE,
+    RS.ENVIRONMENT,
+    RS.GLOBAL_REALM,
+    RS.POSSIBLY_EXTINCT,
+    RS.POSSIBLY_EXTINCT_IN_WILD
+FROM {{ source('silver_source', 'stg_shark_attacks') }} SA
+JOIN {{ source('silver_source', 'stg_shark_species_locations') }} SL
+ON SA.ATTACK_ID = SL.ATTACK_ID
+JOIN {{ source('silver_source', 'stg_red_list_sharks') }} RS
+ON SL.SPECIES = RS.COMMON_NAME
